1d707c43981a3a1c25d94e0433da66c23c0a2c4e
Artemis854 <kaushal197@gmail.com>
2017-10-27 13:07:06 -0400

    minor changes

--- Python/pareto_dominance.py
+++ Python/pareto_dominance.py
@@ -2,31 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Fixing random state for reproducibility
-np.random.seed(928471278)
+np.random.seed(34124)
 
-
-N = 50
+N = 20
 x = np.random.rand(N)
 y = np.random.rand(N)
-colors = ['red']*50
+colors = ['red']*N
 
-x = [ int(i*30) for i in x ]
-y = [ int(i*30) for i in y ]
+x = [ int(i*150) for i in x ]
+y = [ int(i*150) for i in y ]
 
-points = sorted(zip(x, y, colors), key=lambda x:x[1])
+front = []
+
+points = sorted(zip(x, y, colors), key=lambda x:x[1], reverse=True)
 
 front_point = points[0]
 
 for i in range(len(points)):
-	if points[i][0] >= front_point[0]:
+	if points[i][0] <= front_point[0]:
 		front_point = points[i]
 		temp = list(points[i])
 		temp[2] = 'blue'
 		points[i] = tuple(temp)
+		front.append(tuple(temp))
 
 points = zip(*points)
-
 x, y, colors = points
 
-plt.scatter(x, y, s=5, c=colors, alpha=0.5)
+plt.scatter(x, y, s=25, c=colors, alpha=0.5)
+
+front = zip(*front)
+x, y, colors = front
+
+plt.plot(x, y)
 plt.show()
